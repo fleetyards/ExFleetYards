97b0fb2aa5db4fb25cc83feff5a11c4902f199e2
Finn Behrens <me@kloenk.dev>
2022-12-19 16:49:38 +0100

    update config

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :fleet_yards, FleetYards.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "fleet_yards_dev",
+  password: "fleet_yards_dev",
   hostname: "localhost",
   database: "fleet_yards_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
