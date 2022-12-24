79beb10cef00d16aa2b53b45b79aa9a5729c61b3
Finn Behrens <me@kloenk.dev>
2022-12-24 13:28:57 +0100

    rename metrics

--- apps/ex_fleet_yards_web/lib/ex_fleet_yards_web/telemetry.ex
+++ apps/ex_fleet_yards_web/lib/ex_fleet_yards_web/telemetry.ex
@@ -52,23 +52,23 @@ defmodule ExFleetYardsWeb.Telemetry do
       ),
 
       # Database Metrics
-      summary("fleet_yards.repo.query.total_time",
+      summary("ex_fleet_yards.repo.query.total_time",
         unit: {:native, :millisecond},
         description: "The sum of the other measurements"
       ),
-      summary("fleet_yards.repo.query.decode_time",
+      summary("ex_fleet_yards.repo.query.decode_time",
         unit: {:native, :millisecond},
         description: "The time spent decoding the data received from the database"
       ),
-      summary("fleet_yards.repo.query.query_time",
+      summary("ex_fleet_yards.repo.query.query_time",
         unit: {:native, :millisecond},
         description: "The time spent executing the query"
       ),
-      summary("fleet_yards.repo.query.queue_time",
+      summary("ex_fleet_yards.repo.query.queue_time",
         unit: {:native, :millisecond},
         description: "The time spent waiting for a database connection"
       ),
-      summary("fleet_yards.repo.query.idle_time",
+      summary("ex_fleet_yards.repo.query.idle_time",
         unit: {:native, :millisecond},
         description:
           "The time the connection spent waiting before being checked out for the query"
