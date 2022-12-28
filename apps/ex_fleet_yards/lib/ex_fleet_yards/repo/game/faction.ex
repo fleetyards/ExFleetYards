8d14417b58ed175625f89ff09a547af5f31e1b0e
Finn Behrens <me@kloenk.dev>
2022-12-28 21:29:47 +0100

    add affiliations and factions

---
+++ apps/ex_fleet_yards/lib/ex_fleet_yards/repo/game/faction.ex
@@ -0,0 +1,18 @@
+defmodule ExFleetYards.Repo.Game.Faction do
+  @moduledoc "Faction"
+
+  use Ecto.Schema
+  alias ExFleetYards.Repo.Game
+
+  @primary_key {:id, Ecto.UUID, []}
+
+  schema "factions" do
+    field :code, :string
+    field :color, :string
+    field :name, :string
+    field :slug, :string
+    field :rsi_id, :integer
+
+    timestamps(inserted_at: :created_at)
+  end
+end
---
+++ apps/ex_fleet_yards/priv/repo/seeds/20221228201036_affiliations.exs
@@ -0,0 +1,38 @@
+defmodule ExFleetYards.Repo.Seeds.Affiliations do
+  import Seedex
+
+  alias ExFleetYards.Repo
+
+  seed Repo.Game.Affiliation,
+       [:faction_id],
+       [
+         %{
+           faction_id: "uee",
+           affiliationable_id: "stanton",
+           affiliationable_type: "Starsystem"
+         },
+         %{
+           faction_id: "unclaimed",
+           affiliationable_id: "uriel",
+           affiliationable_type: "CelestialObject"
+         }
+       ],
+       fn affiliation ->
+         affiliation
+
+         faction = Repo.get_by!(Repo.Game.Faction, slug: affiliation.faction_id).id
+
+         module =
+           Map.get(affiliation, :affiliationable_type)
+           |> case do
+             "Starsystem" -> Repo.Game.StarSystem
+             "CelestialObject" -> Repo.Game.CelestialObject
+           end
+
+         id = Repo.get_by!(module, slug: Map.get(affiliation, :affiliationable_id)).id
+
+         affiliation
+         |> Map.put(:faction_id, faction)
+         |> Map.put(:affiliationable_id, id)
+       end
+end
