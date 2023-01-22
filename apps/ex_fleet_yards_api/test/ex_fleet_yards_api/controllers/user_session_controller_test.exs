3090561f50759b8dcc14297c2daefd8568e270df
Finn Behrens <me@kloenk.dev>
2023-01-22 11:35:41 +0100

    fix delete_all on UserSessionController

--- apps/ex_fleet_yards_api/test/ex_fleet_yards_api/controllers/user_session_controller_test.exs
+++ apps/ex_fleet_yards_api/test/ex_fleet_yards_api/controllers/user_session_controller_test.exs
@@ -45,5 +45,58 @@ defmodule ExFleetYardsApi.UserSessionControllerTest do
 
       assert json["code"] == "success"
     end
+
+    test "list and logout all", %{conn: conn} do
+      scopes = ExFleetYards.Repo.Account.UserToken.scopes()
+
+      json =
+        conn
+        |> post(Routes.user_session_path(conn, :create), %{
+          "username" => "testuser",
+          "password" => "testuserpassword",
+          "scopes" => scopes
+        })
+        |> json_response(201)
+
+      assert json["code"] == "success"
+      assert json["token"] != ""
+
+      token = json["token"]
+
+      conn =
+        conn
+        |> put_req_header("authorization", "Bearer #{token}")
+
+      json =
+        conn
+        |> post(Routes.user_session_path(conn, :create), %{
+          "username" => "testuser",
+          "password" => "testuserpassword",
+          "scopes" => %{}
+        })
+        |> json_response(201)
+
+      assert json["code"] == "success"
+      assert json["token"] != ""
+
+      json =
+        conn
+        |> get(Routes.user_session_path(conn, :list))
+        |> json_response(200)
+
+      assert json |> Enum.count() == 2
+
+      json =
+        conn
+        |> delete(Routes.user_session_path(conn, :delete_all))
+        |> json_response(200)
+
+      assert json["code"] == "success"
+
+      assert_error_sent 401, fn ->
+        conn
+        |> get(Routes.user_session_path(conn, :list))
+      end
+    end
   end
 end
